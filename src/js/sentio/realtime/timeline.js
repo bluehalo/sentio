f18a4faebb42795a67cef953607995ff36d91dad
reblace <reblace@gmail.com>
2015-06-18 10:45:55 -0400

    [A-30] Fixing some problems with the timeline drawing inaccurately wrt the x scale

--- src/js/sentio/realtime/timeline.js
+++ src/js/sentio/realtime/timeline.js
@@ -27,11 +27,10 @@ function sentio_realtime_timeline() {
 
 	// Is the timeline running?
 	var _running = false;
+	var _firstTime = true;
 
 	// Transition used for normal mode
-	var _transition = d3.select({}).transition()
-		.duration(_duration.reveal)
-		.ease('linear');
+	var _transition;
 
 
 	// Is the timeline running in efficient mode?
@@ -186,16 +185,23 @@ function sentio_realtime_timeline() {
 		var now = new Date();
 
 		// Update the x domain (to the latest time window)
-		_scale.x.domain([now - _delay - _interval - _duration.reveal, now - _delay - _duration.reveal]);
+		_scale.x.domain([now - _delay - _interval, now - _delay - _duration.reveal]);
 
 		// Update the y domain (based on configuration and data)
 		_scale.y.domain(getYExtent(now));
 
 		// Either tick efficiently or normally
 		var efficient = !(null == _efficient || !_efficient.enabled);
+		if(_firstTime) {
+			_firstTime = false;
+			efficient = true;
+			_transition = d3.select({}).transition()
+				.duration(_duration.reveal)
+				.ease('linear');
+		}
 
 		if(efficient) {
-			var translate = '-' + _scale.x(now - _delay - _interval + _duration.reveal);
+			var translate = '-' + _scale.x(now - _delay - _interval);
 			tickAxes(efficient);
 			tickLine(translate, efficient);
 			tickMarkers(translate, efficient);
@@ -204,9 +210,9 @@ function sentio_realtime_timeline() {
 			window.setTimeout(tick, 1000/_efficient.fps);
 		} else {
 			_transition = _transition.each(function(){
-				var translate = _scale.x(now - _delay - _interval - 2*_duration.reveal);
+				var translate = _scale.x(now - _delay - _interval);
 				tickAxes(efficient);
-				tickLine(translate, efficient);
+				tickLine(translate, efficient, now);
 				tickMarkers(translate, efficient);
 			}).transition().each('start', tick);
 		}
@@ -222,13 +228,13 @@ function sentio_realtime_timeline() {
 		}
 	}
 
-	function tickLine(translate, efficient) {
+	function tickLine(translate, efficient, now) {
 		// Select and draw the line
 		var path = _element.g.line.select('.line').attr('d', _line);
 
 		// If we are not in efficient mode, reset the transform and apply a transition
 		if(!efficient) {
-			path = path.attr('transform', null).transition().duration(_transition.duration());
+			path = path.attr('transform', 'translate(' + _scale.x(now - _delay - _interval + _duration.reveal) + ')').transition().duration(_transition.duration());
 		}
 
 		// Set the final transform state
@@ -322,6 +328,7 @@ function sentio_realtime_timeline() {
 	chart.start = function(){
 		if(_running){ return; }
 		_running = true;
+		_firstTime = true;
 
 		tick();
 	};
@@ -416,6 +423,7 @@ function sentio_realtime_timeline() {
 	};
 	chart.efficient = function(v){
 		if(!arguments.length) { return _efficient; }
+		_firstTime = true;
 		_efficient = v;
 		return chart;
 	};
