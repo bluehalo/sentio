99eb4e6b911fb73822d539faf873f5d73aa24f59
Ryan Blace <reblace@gmail.com>
2017-04-11 10:36:53 -0400

    Updating a few format strings in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ let
 
 
 // Banner to append to generated files
-let bannerString = '/*! ' + pkg.name + '-' + pkg.version + ' - ' + pkg.copyright + '*/'
+let bannerString = `/*! ${pkg.name} - ${pkg.version} - ${pkg.copyright} */`;
 
 
 /**
@@ -40,7 +40,7 @@ gulp.task('validate-js', () => {
 gulp.task('build-js', ['rollup-js'], () => {
 
 	// Uglify
-	return gulp.src(path.join(assets.dist.dir, (pkg.artifactName + '.js')))
+	return gulp.src(path.join(assets.dist.dir, `${pkg.artifactName}.js`))
 		.pipe(plugins.uglify({ preserveComments: 'license' }))
 		.pipe(plugins.rename(pkg.artifactName + '.min.js'))
 		.pipe(gulp.dest(assets.dist.dir));
@@ -53,7 +53,7 @@ gulp.task('rollup-js', () => {
 		})
 		.then((bundle) => {
 			return bundle.write({
-				dest: path.join(assets.dist.dir, (pkg.artifactName + '.js')),
+				dest: path.join(assets.dist.dir, `${pkg.artifactName}.js`),
 				format: 'umd',
 				moduleName: 'sentio',
 				sourceMap: true,
